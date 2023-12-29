584b045e9183720942ee569081838c9e34d9b426
Safak Arica <safak.arica@algbra.com>
2023-12-29 14:56:09 +0300

    test 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
+
+const tina = ({ directiveName = "tina" } = {}) => ({
+  name: "tina-cms",
+  hooks: {
+    "astro:config:setup": ({ addClientDirective, opts }) => {
+      addClientDirective({
+        name: directiveName,
+        entrypoint: "./client-directives/tina.mjs",
+      });
+    },
+  },
+});
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind(), react(), tina()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
