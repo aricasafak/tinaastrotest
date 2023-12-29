9f6e774352646d0cc93b31f0d082053945e8a608
Safak Arica <safak.arica@algbra.com>
2023-12-29 13:47:25 +0300

    test 6

--- .tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'd77a16071597e7125dd5eb671d4571195eb8b5b6', queries,  });
-export default client;
-  
